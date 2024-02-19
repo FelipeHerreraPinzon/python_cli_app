3ca27b6cc0aec0b81d9b376139c993aabbc5698e
unknown <felipeherrerapinzon@gmail.com>
2024-02-19 09:54:50 -0500

    terminado

--- agenda.py
+++ agenda.py
@@ -31,6 +31,47 @@ def users():
         print(f"{user['id']} - {user['nombre']} - {user['apellido']}")
 
 
+@cli.command()
+@click.argument('id', type=int)
+@click.option('--nombre', help='Nombre del usuario')
+@click.option('--apellido', help='Apellido del usuario')
+def update(id, nombre, apellido):
+    users = json_manager.read_json()
+    for user in users:
+        if user['id'] == id:
+            if nombre is not None:
+                user['nombre'] = nombre
+            if apellido is not None:
+                user['apellido'] = apellido
+            break    
+    json_manager.write_json(users)    
+    print(f"User with id {id} updated successfully")
+
+
+@cli.command()
+@click.argument('id', type=int)
+def user(id):
+    users = json_manager.read_json()
+    user = next((user for user in users if user['id'] == id), None)
+    if user is None:
+        print(f"User with id {id} not found")
+    else:    
+        print(f"{user['id']} - {user['nombre']} - {user['apellido']}")
+
+
+@cli.command()
+@click.argument('id', type=int)
+def delete(id):
+    users = json_manager.read_json()
+    userDelete = next((user for user in users if user['id'] == id), None)
+    if userDelete is None:
+        print(f"User with id {id} not found")
+    else:    
+        users.remove(userDelete)
+        json_manager.write_json(users)
+        print(f"User with id {id} was deleted")
+
+
 if __name__ == '__main__':
     cli()
 
